34e32241fea67f6ba10bd3d6e71098720f06782f
dodo0211 <minar2010@naver.com>
2021-03-25 19:48:08 +0900

    #python: 주식차트 크롤링
    Yahoo finance에서 RIOT 2020년 9월~12월 일일 주가 정보 크롤링
    
    beautifulSoup 라이브러리 이용

--- python/main.py
+++ python/main.py
@@ -1,15 +1,69 @@
 from bs4 import BeautifulSoup
-from urllib.request import urlopen
+import requests
 
-print('hh')
-html = urlopen("https://finance.naver.com/item/main.nhn?code=005930")
-print('hh')
+# url 2020년 1월1일부터 2020년 12월 31일 까지 설정한 상태 (총 252일)
+url = "https://finance.yahoo.com/quote/RIOT/history?period1=1577836800&period2=1609372800&interval=1d&filter=history&frequency=1d&includeAdjustedClose=true"
 
-bsObject = BeautifulSoup(html, "html.parser")
-print('hh')
+response = requests.get(url)
 
-no_today = bsObject.find("p", {"class":"no_today"})
-print('hh')
-print(no_today)
+if response.status_code == 200:
 
-print('hh')
+    html = response.text
+    soup = BeautifulSoup(html, 'html.parser')
+
+    # 4개월씩 3개
+    i = 0
+    for i in range(1, 84):   #12월~9월(4개월)
+        print(i)
+
+        date = soup.select_one(
+            '#Col1-1-HistoricalDataTable-Proxy > section > div.Pb\(10px\).Ovx\(a\).W\(100\%\) > table > tbody > tr:nth-child('+ str(i) +') > td.Py\(10px\).Ta\(start\).Pend\(10px\) > span')
+        if date is not None:
+            print(date.get_text)
+        else:
+            print(date)
+
+        open = soup.select_one(
+            '#Col1-1-HistoricalDataTable-Proxy > section > div.Pb\(10px\).Ovx\(a\).W\(100\%\) > table > tbody > tr:nth-child('+ str(i) +') > td:nth-child(2) > span')
+        if open is not None:
+            print(open.get_text())
+        else:
+            print(open)
+
+        high = soup.select_one(
+            '#Col1-1-HistoricalDataTable-Proxy > section > div.Pb\(10px\).Ovx\(a\).W\(100\%\) > table > tbody > tr:nth-child('+ str(i) +') > td:nth-child(3) > span')
+        if high is not None:
+            print(high.get_text())
+        else:
+            print(high)
+
+        low = soup.select_one(
+            '#Col1-1-HistoricalDataTable-Proxy > section > div.Pb\(10px\).Ovx\(a\).W\(100\%\) > table > tbody > tr:nth-child('+ str(i) +') > td:nth-child(4) > span')
+        if low is not None:
+            print(low.get_text())
+        else:
+            print(low)
+
+        close = soup.select_one(
+            '#Col1-1-HistoricalDataTable-Proxy > section > div.Pb\(10px\).Ovx\(a\).W\(100\%\) > table > tbody > tr:nth-child('+ str(i) +') > td:nth-child(5) > span')
+        if close is not None:
+            print(close.get_text())
+        else:
+            print(close)
+
+        volume = soup.select_one(
+            '#Col1-1-HistoricalDataTable-Proxy > section > div.Pb\(10px\).Ovx\(a\).W\(100\%\) > table > tbody > tr:nth-child('+ str(i) +') > td:nth-child(7) > span')
+        if volume is not None:
+            print(volume.get_text())
+        else:
+            print(volume)
+
+        print("\n")
+
+else:
+    print(response.status_code)
+
+    wwww.mm.dd
+    String: date
+    double: 4
+    int: 1
