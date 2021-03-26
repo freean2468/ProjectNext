9f7684ea91baf90d3ac664866ab6d9b459796bd0
dodo0211 <minar2010@naver.com>
2021-03-26 15:10:06 +0900

    #python: 주식차트 크롤링
    Yahoo finance에서 RIOT 2020년 9월~12월 일일 주가 정보 크롤링
    
    beautifulSoup 라이브러리만으로는 스크롤을 내려서 로딩해오는 것에 문제가 있어서 selenium 라이브러리와 함께 사용하였다.
    
    스크롤의 전체길이를 측정하고 로딩시간을 준 뒤 로딩이 되면 다시 스크롤의 전체 길이를 측정한다.
    스크롤의 원래길이와 대기시간을 갖어서 로딩을 한 후의 스크롤의 길이가 같으면 while문에서 빠져나온다.

--- python/main.py
+++ python/main.py
@@ -1,69 +1,89 @@
+import time
+
+from selenium import webdriver
 from bs4 import BeautifulSoup
-import requests
 
-# url 2020년 1월1일부터 2020년 12월 31일 까지 설정한 상태 (총 252일)
-url = "https://finance.yahoo.com/quote/RIOT/history?period1=1577836800&period2=1609372800&interval=1d&filter=history&frequency=1d&includeAdjustedClose=true"
+SCROLL_PAUSE_SEC = 0.2
 
-response = requests.get(url)
+# selenium에서 사용할 웹 드라이버 절대 경로 정보
+driverChrome = 'C:\\Users\\mrhi6-10\\Downloads\\chromedriver_win32\\chromedriver'
+# selenium의 webdriver에 앞서 설지한 chromediriver를 연동
+driver = webdriver.Chrome(driverChrome)
 
-if response.status_code == 200:
+# driver로 특정 페이지를 크롤링
+driver.get('https://finance.yahoo.com/quote/RIOT/history?period1=1577836800&period2=1609372800&interval=1d&filter=history&frequency=1d&includeAdjustedClose=true')
 
-    html = response.text
-    soup = BeautifulSoup(html, 'html.parser')
+last_height = 0
+new_height = 0
 
-    # 4개월씩 3개
-    i = 0
-    for i in range(1, 84):   #12월~9월(4개월)
-        print(i)
+for i in range(1, 5):
+    # 스크롤 높이에 3000만큼 더 가져옴
+    new_height += 3000
 
-        date = soup.select_one(
-            '#Col1-1-HistoricalDataTable-Proxy > section > div.Pb\(10px\).Ovx\(a\).W\(100\%\) > table > tbody > tr:nth-child('+ str(i) +') > td.Py\(10px\).Ta\(start\).Pend\(10px\) > span')
-        if date is not None:
-            print(date.get_text)
-        else:
-            print(date)
+    # 끝까지 스크롤 다운
+    driver.execute_script("window.scrollTo("+str(last_height)+", "+str(new_height)+");")
 
-        open = soup.select_one(
-            '#Col1-1-HistoricalDataTable-Proxy > section > div.Pb\(10px\).Ovx\(a\).W\(100\%\) > table > tbody > tr:nth-child('+ str(i) +') > td:nth-child(2) > span')
-        if open is not None:
-            print(open.get_text())
-        else:
-            print(open)
+    time.sleep(SCROLL_PAUSE_SEC)
 
-        high = soup.select_one(
-            '#Col1-1-HistoricalDataTable-Proxy > section > div.Pb\(10px\).Ovx\(a\).W\(100\%\) > table > tbody > tr:nth-child('+ str(i) +') > td:nth-child(3) > span')
-        if high is not None:
-            print(high.get_text())
-        else:
-            print(high)
+########################################################################################################
 
-        low = soup.select_one(
-            '#Col1-1-HistoricalDataTable-Proxy > section > div.Pb\(10px\).Ovx\(a\).W\(100\%\) > table > tbody > tr:nth-child('+ str(i) +') > td:nth-child(4) > span')
-        if low is not None:
-            print(low.get_text())
-        else:
-            print(low)
+html = driver.page_source
+soup = BeautifulSoup(html, 'html.parser')
 
-        close = soup.select_one(
-            '#Col1-1-HistoricalDataTable-Proxy > section > div.Pb\(10px\).Ovx\(a\).W\(100\%\) > table > tbody > tr:nth-child('+ str(i) +') > td:nth-child(5) > span')
-        if close is not None:
-            print(close.get_text())
-        else:
-            print(close)
+i = 0
+while 1 :
+    i = i + 1
 
-        volume = soup.select_one(
-            '#Col1-1-HistoricalDataTable-Proxy > section > div.Pb\(10px\).Ovx\(a\).W\(100\%\) > table > tbody > tr:nth-child('+ str(i) +') > td:nth-child(7) > span')
-        if volume is not None:
-            print(volume.get_text())
-        else:
-            print(volume)
+    print(i)
 
-        print("\n")
+    date = soup.select_one(
+        '#Col1-1-HistoricalDataTable-Proxy > section > div.Pb\(10px\).Ovx\(a\).W\(100\%\) > table > tbody > tr:nth-child('+ str(i) +') > td.Py\(10px\).Ta\(start\).Pend\(10px\) > span')
+    if date is not None:
+        print(date.get_text())
+    else:
+        print(date)
+        break
 
-else:
-    print(response.status_code)
+    open = soup.select_one(
+        '#Col1-1-HistoricalDataTable-Proxy > section > div.Pb\(10px\).Ovx\(a\).W\(100\%\) > table > tbody > tr:nth-child('+ str(i) +') > td:nth-child(2) > span')
+    if open is not None:
+        print(open.get_text())
+    else:
+        print(open)
+        break
 
-    wwww.mm.dd
-    String: date
-    double: 4
-    int: 1
+    high = soup.select_one(
+        '#Col1-1-HistoricalDataTable-Proxy > section > div.Pb\(10px\).Ovx\(a\).W\(100\%\) > table > tbody > tr:nth-child('+ str(i) +') > td:nth-child(3) > span')
+    if high is not None:
+        print(high.get_text())
+    else:
+        break
+        print(high)
+
+    low = soup.select_one(
+        '#Col1-1-HistoricalDataTable-Proxy > section > div.Pb\(10px\).Ovx\(a\).W\(100\%\) > table > tbody > tr:nth-child('+ str(i) +') > td:nth-child(4) > span')
+    if low is not None:
+        print(low.get_text())
+    else:
+        print(low)
+        break
+
+    close = soup.select_one(
+        '#Col1-1-HistoricalDataTable-Proxy > section > div.Pb\(10px\).Ovx\(a\).W\(100\%\) > table > tbody > tr:nth-child('+ str(i) +') > td:nth-child(5) > span')
+    if close is not None:
+        print(close.get_text())
+    else:
+        print(close)
+        break
+
+    volume = soup.select_one(
+        '#Col1-1-HistoricalDataTable-Proxy > section > div.Pb\(10px\).Ovx\(a\).W\(100\%\) > table > tbody > tr:nth-child('+ str(i) +') > td:nth-child(7) > span')
+    if volume is not None:
+        print(volume.get_text())
+    else:
+        print(volume)
+        break
+
+    print("\n")
+
+print('end of loading')
