914164f0f6207003177f1ac2d87391fdf85d3f5f
dodo0211 <minar2010@naver.com>
2021-04-01 19:25:47 +0900

    #python: 종목 추가
    Nokia Corporation (NOK),
    GameStop Corp. (GME),
    Tesla, Inc. (TSLA)
    
    2020년 1월 1일 ~ 2020년 12월 31일

--- python/main.py
+++ python/main.py
@@ -2,17 +2,27 @@ import core
 
 # url 2020년 1월1일부터 2020년 12월 31일 까지 설정한 상태
 targetUrl = [
-    #RIOT
+    # Riot Blockchain, Inc. (RIOT)
     "https://finance.yahoo.com/quote/RIOT/history?period1=1577836800&period2=1609372800&interval=1d&filter=history&frequency=1d&includeAdjustedClose=true",
 
-    # NASDAQ
+    # NASDAQ Composite (^IXIC)
     "https://finance.yahoo.com/quote/%5EIXIC/history?period1=1577836800&period2=1609372800&interval=1d&filter=history&frequency=1d&includeAdjustedClose=true",
+
+    # Apple Inc. (AAPL) - 3년치 데이터
+    # "https://finance.yahoo.com/quote/AAPL/history?period1=1514764800&period2=1609372800&interval=1d&filter=history&frequency=1d&includeAdjustedClose=true",
     
-    # APPLE - 3년치 데이터
-    # "https://finance.yahoo.com/quote/AAPL/history?period1=1514764800&period2=1609372800&interval=1d&filter=history&frequency=1d&includeAdjustedClose=true"
-    
-    # APPLE - 1년치 데이터
-    "https://finance.yahoo.com/quote/AAPL/history?period1=1577836800&period2=1609372800&interval=1d&filter=history&frequency=1d&includeAdjustedClose=true"
+    # Apple Inc. (AAPL) - 1년치 데이터
+    "https://finance.yahoo.com/quote/AAPL/history?period1=1577836800&period2=1609372800&interval=1d&filter=history&frequency=1d&includeAdjustedClose=true",
+
+    # Nokia Corporation (NOK)
+    "https://finance.yahoo.com/quote/NOK/history?period1=1577836800&period2=1609372800&interval=1d&filter=history&frequency=1d&includeAdjustedClose=true",
+
+    # GameStop Corp. (GME)
+    "https://finance.yahoo.com/quote/GME/history?period1=1577836800&period2=1609372800&interval=1d&filter=history&frequency=1d&includeAdjustedClose=true",
+
+    # Tesla, Inc. (TSLA)
+    "https://finance.yahoo.com/quote/TSLA/history?period1=1577836800&period2=1609372800&interval=1d&filter=history&frequency=1d&includeAdjustedClose=true"
+
 ]
 
 core.crawling(targetUrl)
